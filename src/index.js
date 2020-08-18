4fe81c014d1adb62d6e4faaf4d5ce322d01d77ed
Femi Lawal <lawalshalom@gmail.com>
2020-08-18 11:54:38 +0100

    countdown to launch

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <BrowserRouter>
   <div className="section-bg"></div>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
 )
-
+*/
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
