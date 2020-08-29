8d447ef5e5545e1bd68b5a1faaf33a90c37c23f4
Femi Lawal <lawalshalom@gmail.com>
2020-08-29 02:06:37 +0100

    1 day to go

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Placeholder = () => {
     return (
         <div className="text-center" style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", background: "#fff"}}>
-            <h2><strong>5 days to launch!</strong></h2>
-            <img src="countdown.jpeg" alt="placeholder-text" style={{width: "100%"}}/>
+            <h2><strong>1 day to launch!</strong></h2>
+            <img src="countdown1.jpeg" alt="placeholder-text" style={{width: "100%"}}/>
         </div>
     )
 
