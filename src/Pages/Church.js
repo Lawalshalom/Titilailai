ab9ac9452ffad24c973c12f9f9414e3d22ced2b0
Femi Lawal <lawalshalom@gmail.com>
2020-10-30 08:04:08 +0100

    view fixes

--- src/Pages/Church.js
+++ src/Pages/Church.js
@@ -7,7 +7,8 @@ export default function Church() {
         <div className="church">
             <Header page="church" />
             <div className="cont d-flex flex-column justify-content-center">
-                <div className="hero container d-flex flex-column align-items-center">
+                <div className="hero container d-flex flex-column flex-md-row flex-lg-row align-items-center">
+                <div className="col-12 col-md-6 col-lg-6 d-flex flex-column align-items-center">
                     <h1 className="text-center">Church<br/>
                         <span>Wedding</span></h1>
 
@@ -19,6 +20,12 @@ export default function Church() {
                         <h3><strong>Time:</strong> 10am</h3>
                     </div>
                 </div>
+                <div className="church-map col-12 col-md-6 col-lg-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.8916549267115!2d3.861466050285119!3d7.3660399946586095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10398d2c4c64cea1%3A0x60972181788e0b99!2sNew%20Covenant%20Church!5e0!3m2!1sen!2sng!4v1604040246224!5m2!1sen!2sng" width="600" height="450" frameborder="0" style={{border:0}} title="church map" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+                </div>
+                </div>
+
            </div>
            <Footer />
         </div>
