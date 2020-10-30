31e07674dee8a2d388617fa8e381a6788afcaf53
Femi Lawal <lawalshalom@gmail.com>
2020-10-30 14:59:46 +0100

    carousel fixes

--- src/Pages/Photos.js
+++ src/Pages/Photos.js
@@ -7,79 +7,73 @@ import Footer from "../Components/Footer";
 
 export default function Photos() {
     return (
-        <div>
+        <div className="photos">
             <Header page="photos" />
-            <div className="gallery text-center">
+            <div className="gallery">
 
-            <div id="carousel-example" className="carousel slide d-flex" data-ride="carousel">
-                        <div className="carousel-inner mx-auto row">
+                <div id="carousel-example" className="carousel slide row" data-pause="true" data-ride="false">
+                    <div className="carousel-inner col-sm-12 col-md-10 col-lg-6">
 
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div className="carousel-item">
                             <img className="d-block w-100" src="images/1.webp" alt="Prefirst slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/2.webp" alt="First slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3 active">
-                            <img className="d-block w-100" src="images/3.webp" alt="Second slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/4.webp" alt="Third slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/5.webp" alt="Fourth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/6.webp" alt="Fifth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/7.webp" alt="Sixth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/8.webp" alt="Seventh slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/9.webp" alt="Eighth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/10.webp" alt="Ninth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/11.webp" alt="Tenth slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/12.webp" alt="Eleventh slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/13.webp" alt="Twelve slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/14.webp" alt="Eleventh slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/15.webp" alt="Eleventh slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/16.webp" alt="Eleventh slide"/>
-                          </div>
-                          <div className="carousel-item  col-12 col-sm-6 col-md-4 col-lg-3">
-                            <img className="d-block w-100" src="images/17.webp" alt="Eleventh slide"/>
-                          </div>
-
-
                         </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/2.webp" alt="First slide"/>
+                        </div>
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src="images/3.webp" alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/4.webp" alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/5.webp" alt="Fourth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/6.webp" alt="Fifth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/7.webp" alt="Sixth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/8.webp" alt="Seventh slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/9.webp" alt="Eighth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/10.webp" alt="Ninth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/11.webp" alt="Tenth slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/12.webp" alt="Eleventh slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/13.webp" alt="Twelve slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/14.webp" alt="Eleventh slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/15.webp" alt="Eleventh slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="images/17.webp" alt="Eleventh slide"/>
+                        </div>
+
                     </div>
+                </div>
 
-
-                    <a class="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-
+                <a className="carousel-control-prev" href="#carousel-example" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carousel-example" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
 
             </div>
             <Footer />
